d1bd09acef81e970bd00324152045be3bc518ab6
Marco Zerbinati <zerbinati.marco@gmail.com>
2019-06-30 16:41:53 +0200

    Fixed name

--- src/misc.cpp
+++ src/misc.cpp
@@ -127,7 +127,7 @@ const string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "SugaR-ND " << Version << setfill('0');
+  ss << "SugaR-NN " << Version << setfill('0');
 
   if (Version.empty())
   {
